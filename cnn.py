000e4febd8c2f47cf7463360e9afb62adb49f8ea
smalldatabrains <smalldatabrains@gmail.com>
2017-02-08 16:19:07 +0700

    bug correction

--- cnn.py
+++ cnn.py
@@ -38,6 +38,28 @@ output=np.asarray(output)
 print(input.shape)
 print(output.shape)
 
+#preparing training an testing set
+permutation = np.random.permutation(input.shape[0]) #shuffling the rows
+input=input[permutation]
+output=output[permutation]
+train_set=input[0:int(0.8*input.shape[0])]
+train_label=output[0:int(0.8*input.shape[0])]
+test_set=input[int(0.8*input.shape[0])+1:input.shape[0]]
+test_label=output[int(0.8*output.shape[0])+1:output.shape[0]]
+
+#parameters
+img_size=120
+img_size_flat=img_size*img_size
+img_shape=(img_size,img_size)
+num_channels=1
+num_classes=2
+
+filter_size1 = 5   
+num_filters1 = 16        
+filter_size2 = 5      
+num_filters2 = 36 
+fc_size = 128       
+
 #Initialization of Weights and biases
 def Weight(shape):
 	return tf.Variable(tf.truncated_normal(shape,stddev=0.1))
@@ -45,8 +67,8 @@ def bias(length):
 	return tf.Variable(tf.constant(0.1,shape=[length]))
 
 #convolutional layer creation
-def convolutional_layer(input,input_channels,filter_size,num_filters,use_pooling=True):
-	shape=[filter_size,filter_size,input_channels,num_filters]
+def convolutional_layer(input,num_input_channels,filter_size,num_filters,use_pooling=True):
+	shape=[filter_size,filter_size,num_input_channels,num_filters]
 	weights=Weight(shape=shape)
 	biases=bias(length=num_filters)
 	layer=tf.nn.conv2d(input=input,filter=weights,strides=[1,1,1,1],padding='SAME')+biases
@@ -56,11 +78,11 @@ def convolutional_layer(input,input_channels,filter_size,num_filters,use_pooling
 	return layer,weights
 
 #flatten layer
-def flatten_layer(conv_layer):
-	shape=conv_layer.get_shape()
-	num_features=np.array(shape[1:4],dtype=int).prod()
-	layer_flat=tf.reshape(conv_layer,[-1,num_features])
-	return layer_flat
+def flatten_layer(layer):
+	layer_shape=layer.get_shape()
+	num_features=layer_shape[1:4].num_elements()
+	layer_flat=tf.reshape(layer,[-1,num_features])
+	return layer_flat,num_features
 
 #fully connected layer
 def fc_layer(input,num_inputs,num_outputs,use_relu=True):
@@ -72,22 +94,44 @@ def fc_layer(input,num_inputs,num_outputs,use_relu=True):
 	return layer
 
 #placeholders
-X=tf.placeholder(tf.float32,shape=[-1,120,120,1])
-Yreal=tf.placeholder(tf.float32,shape=[None,2])
+X=tf.placeholder(tf.float32,shape=[None,img_size_flat],name='X')
+X_image=tf.reshape(X,[-1,img_size,img_size,num_channels])
+Yreal=tf.placeholder(tf.float32,shape=[None,2],name='Yreal')
+Yreal_cls=tf.argmax(Yreal,dimension=1)
 
-def Yth(input):
-	X=tf.reshape(input,shape=[-1,120,120,1])
-	Y1=convolutional_layer(X,1,5,16,True)
-	Y2=convolutional_layer(Y1,1,5,36,True)
-	Yflatten=flatten_layer(Y2)
-	Yfc=fc_layer(Yflatten,Y2.shape,2,True)
-	Yth=tf.softmax(Yfc)
-	return Yth
+#layers creation
+layer_conv1, weights_conv1 = \
+    convolutional_layer(input=X_image,
+                   num_input_channels=num_channels,
+                   filter_size=filter_size1,
+                   num_filters=num_filters1,
+                   use_pooling=True)
+
+layer_conv2, weights_conv2 = \
+    convolutional_layer(input=layer_conv1,
+                   num_input_channels=num_filters1,
+                   filter_size=filter_size2,
+                   num_filters=num_filters2,
+                   use_pooling=True)
+
+layer_flat, num_features = flatten_layer(layer_conv2)
+
+layer_fc1 = fc_layer(input=layer_flat,
+                         num_inputs=num_features,
+                         num_outputs=fc_size,
+                         use_relu=True)
+
+layer_fc2 = fc_layer(input=layer_fc1,
+                         num_inputs=fc_size,
+                         num_outputs=num_classes,
+                         use_relu=False)
+
+Yth = tf.nn.softmax(layer_fc2)
+Yth_cls = tf.argmax(Yth, dimension=1)
 
 #cost function and gradient descent
-Yth=Yth(X)
-learning_rate=0.05
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Yth, labels=Yreal))
+learning_rate=0.003
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2, labels=Yreal))
 train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
 
 #---------------------------------------------------------------------------------------------------------------------
@@ -97,20 +141,17 @@ sess=tf.Session()
 init=tf.global_variables_initializer()
 sess.run(init)
 
-for epoch in range(0,10000):
+for epoch in range(0,10):
 	permutation=np.random.permutation(19600)
 	permutation=permutation[0:100]
-	batch=[input[permutation],output[permutation]]
+	batch=[train_set[permutation],train_label[permutation]]
 	training,loss_val=sess.run([train_step,cross_entropy],feed_dict={X:batch[0],Yreal:batch[1]})
 	print("epoch" ,epoch ,"is being processed")
-	print(loss_val,W[1].eval(session=sess))
+	print(loss_val)
 
 
 #performance measurement
 correct_prediction = tf.equal(tf.argmax(Yth,1), tf.argmax(Yreal,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-prediction=Yth
-print(sess.run(prediction, feed_dict={X: test_set, Yreal: test_label,keep_prob:1}))
-print(sess.run(accuracy, feed_dict={X: test_set, Yreal: test_label,keep_prob:1}))
-print(sess.run(accuracy, feed_dict={X: train_set, Yreal: train_label,keep_prob:1}))
+print(sess.run(accuracy, feed_dict={X: test_set, Yreal: test_label}))
 sess.close()
